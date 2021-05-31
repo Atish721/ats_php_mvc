09ed266136fd2680726f5d6d9211a3a7b381fdd6
Atish Chandole <atish.chandole@bakuun.com>
2021-05-31 20:30:25 +0530

    Add  author

--- system/helpers/link.php
+++ system/helpers/link.php
@@ -7,20 +7,11 @@
  */
 
 function linkCSS($cssPath){
-
-    
     $url = BASEURL . "/" .$cssPath;
     echo '<link rel="stylesheet" href="'. $url .'">';
-
-
 }
 
 function linkJS($jsPath){
-
     $url = BASEURL. "/". $jsPath;
     echo '<script src="'. $url .'"></script>';
 }
-
-
-
-?>
