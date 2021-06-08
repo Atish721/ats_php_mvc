852ffb9a1f7575a912f71d08fce002ae2d67b703
Atish Chandole <atish.chandole@bakuun.com>
2021-06-08 23:31:30 +0530

    update in index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
  * @since       31 May 2021
  */
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-session_start();
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+// session_start();
 include "application/config/config.php";
 include "system/init.php";
 
