f890f35bd8517730a789e49c090122b38f2c3378
Atish Chandole <auto91akc@gmail.com>
2021-07-24 23:10:49 +0530

    Update Applicatioin/config.php file

--- application/config/config.php
+++ application/config/config.php
@@ -2,25 +2,27 @@
 <?php
 
 /**
- * @package		ATS PHP MVC
- * @author		Atish Chandole
+ * @package        ATS PHP MVC
+ * @author        Atish Chandole
  * @since       31 May 2021
  */
 
 // ---------------Set Base URL----------------
-function baseUrl(){
-	if (isset($_SERVER['HTTP_HOST'])) {
-		$http = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
-		$hostname = $_SERVER['HTTP_HOST'];
-		$dir =  str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+function baseUrl()
+{
+    if (isset($_SERVER['HTTP_HOST'])) {
+        $http = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
+        $hostname = $_SERVER['HTTP_HOST'];
+        $dir = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
-		$tmplt = "%s://%s%s";
-		$end = $dir;
-		$baseUrl = sprintf( $tmplt, $http, $hostname, $end );
-	}
-	else $baseUrl = 'http://localhost/';
+        $tmplt = "%s://%s%s";
+        $end = $dir;
+        $baseUrl = sprintf($tmplt, $http, $hostname, $end);
+    } else {
+        $baseUrl = 'http://localhost/';
+    }
 
-	return $baseUrl;
+    return $baseUrl;
 }
 
 $baseUrl = baseUrl();
